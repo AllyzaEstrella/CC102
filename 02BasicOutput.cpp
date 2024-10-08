f8747a5d3a4f8b747884a7d92495682a9f6b97b8
AllyzaEstrella <allyzanicoleestrella@gmail.com>
2024-10-08 09:37:00 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout<< "OUR LADY OF LOURDES COLLEGE FOUNDATION";
+	cout<< "COLLEGE OF COMPUTER SCIENCE";
+	cout<< "BSIT 1-A";
+	cout<< "ALLYZA NICOLE ESTRELLA";
+	cout<< "P-6 PARANG JOSE PANGANIBAN CAMARINES NORTE";
+	
+	return 0;
+}	
---
+++ 03VariableDeclaration.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int age;
+	
+	cout<< "Enter your age:"<< age;
+	cin>> age;
+	cout<< "thank you!"<< endl;
+	
+	return 0;
+}

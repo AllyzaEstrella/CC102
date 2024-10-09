d38802d62694207cb64258b235503c708b2ac3f7
AllyzaEstrella <allyzanicoleestrella@gmail.com>
2024-10-09 09:36:15 +0800

    Add files via upload

---
+++ 05DataTypesOperator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int num1, num2;
+	
+	cout << "put first number:";
+	cin >> num1;
+	cout << "put second Number:";
+	cin >> num2;
+	
+	cout << "Sum" << num1 + num2 << endl;
+	cout << "Difference" << num1 - num2 << endl;
+	cout << "Qoutient" << num1 * num2 << endl;
+	cout << "Product" << num1 / num2 << endl;
+	cout << "Remainder" << num1 % num2 << endl;
+	
+	return 0;
+}
+
+	
+	
+	
+	

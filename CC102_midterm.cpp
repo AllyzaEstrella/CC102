3b5b82f9253be6611fb5f7715ce794fbfc88a2e5
AllyzaEstrella <allyzanicoleestrella@gmail.com>
2024-10-15 10:09:18 +0800

    Add files via upload

---
+++ CC102_midterm.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace
+
+int main()
+
+{ float sugarPriceUSD,ricePricePound,sardinesPricePound,coffeePriceUSD,milkPriceUSD;
+	int sugarQty,riceQty,sardinesQty,coffeeQty,milkQty;
+	int sugarcostUSD,ricecostPound,sardinescostPound,coffeecostUSD,milkcostUSD;
+	
+	 int >> "Enter procedure sugar,rice,sardines,coffee,milk"
+	
+	cout << "sugar cost Pound" << endl;
+	cin << sugarqty;
+	cout << "rice cost Pound" << endl;
+	cin >> riceqty;
+	cout >> "sardines cost Pound" << endl;
+	cin >> sardinesqty;
+	cout << "coffee cost Pound" << endl;
+	cin >> coffeeqty;
+	cout << "milk cost Pound" << endl;
+	cin >>milkqty;
+	
+	return 0;
+}	
